735848a6190b3222e800c258b64d065ce4bbf1e8
aestevca <aestevca@mercadona.es>
2025-07-24 10:49:10 +0200

    refactor directive

--- src/app/presentation/directives/limit-clicks/limit-clicks.directive.ts
+++ src/app/presentation/directives/limit-clicks/limit-clicks.directive.ts
@@ -1,16 +1,13 @@
 import {
   Directive,
-  Input,
   HostListener,
-  ElementRef,
-  Renderer2,
-  OnDestroy,
-  inject,
-  WritableSignal,
-  signal,
   effect,
-  InputSignal,
-  input
+  input,
+  output,
+  signal,
+  type InputSignal,
+  type OutputEmitterRef,
+  type WritableSignal
 } from '@angular/core';
 import { Subject } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -21,70 +18,31 @@ import { debounceTime } from 'rxjs/operators';
   standalone: true
 })
 export class LimitClicksDirective {
-  readonly maxClicks: InputSignal<number> = input.required();
+  readonly maxClicks: InputSignal<number> = input.required<number>({ alias: 'appLimitClicks' });
+  readonly disabledState: OutputEmitterRef<boolean> = output<boolean>();
 
-  readonly #el: ElementRef = inject(ElementRef);
-  readonly #renderer: Renderer2 = inject(Renderer2);
   readonly #clicks: WritableSignal<number> = signal(0);
-  readonly #clickSubject = new Subject<void>();
+  readonly #clickSubject: Subject<void> = new Subject<void>();
 
   readonly #debouncedClick = toSignal(this.#clickSubject.pipe(debounceTime(60000)), { initialValue: null });
-
-  #tooltip: HTMLElement | null = null;
 
   constructor() {
     effect(() => {
       const trigger = this.#debouncedClick();
       if (trigger !== null) {
         this.#clicks.set(0);
-        this.#enableElement();
-        this.#hideTooltip();
+        this.disabledState.emit(false);
       }
     });
   }
 
   @HostListener('click')
-  onClick() {
-    this.#clicks.update((c) => c + 1);
+  onClick(): void {
+    this.#clicks.update((c: number): number => c + 1);
     this.#clickSubject.next();
 
     if (this.#clicks() >= this.maxClicks()) {
-      this.#disableElement();
-      this.#showTooltip('Se ha alcanzado el límite de clics');
-    }
-  }
-
-  #disableElement() {
-    this.#renderer.setProperty(this.#el.nativeElement, 'disabled', true);
-    this.#renderer.setStyle(this.#el.nativeElement, 'background-color', 'grey');
-    this.#renderer.setStyle(this.#el.nativeElement, 'cursor', 'not-allowed');
-  }
-
-  #enableElement() {
-    this.#renderer.setProperty(this.#el.nativeElement, 'disabled', false);
-    this.#renderer.setStyle(this.#el.nativeElement, 'background-color', '');
-    this.#renderer.setStyle(this.#el.nativeElement, 'cursor', 'pointer');
-  }
-
-  #showTooltip(message: string) {
-    if (!this.#tooltip) {
-      this.#tooltip = this.#renderer.createElement('span');
-      const text = this.#renderer.createText(message);
-      this.#renderer.appendChild(this.#tooltip, text);
-      this.#renderer.appendChild(this.#el.nativeElement.parentNode, this.#tooltip);
-      this.#renderer.setStyle(this.#tooltip, 'position', 'absolute');
-      this.#renderer.setStyle(this.#tooltip, 'background-color', 'black');
-      this.#renderer.setStyle(this.#tooltip, 'color', 'white');
-      this.#renderer.setStyle(this.#tooltip, 'padding', '5px');
-      this.#renderer.setStyle(this.#tooltip, 'border-radius', '3px');
-      this.#renderer.setStyle(this.#tooltip, 'z-index', '1000');
-    }
-  }
-
-  #hideTooltip() {
-    if (this.#tooltip) {
-      this.#renderer.removeChild(this.#el.nativeElement.parentNode, this.#tooltip);
-      this.#tooltip = null;
+      this.disabledState.emit(true);
     }
   }
 }
--- src/app/presentation/pages/directive/directive.component.ts
+++ src/app/presentation/pages/directive/directive.component.ts
@@ -1,14 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, WritableSignal, signal } from '@angular/core';
 import { LimitClicksDirective } from '../../directives/limit-clicks/limit-clicks.directive';
 import { CommonModule } from '@angular/common';
-import { NavigationComponent } from "@/components/navigation/navigation.component";
+import { NavigationComponent } from '@/components/navigation/navigation.component';
+import { MButtonComponent } from '@mercadona/components/button';
 
 @Component({
   selector: 'app-directive',
   templateUrl: './directive.component.html',
   styleUrls: ['./directive.component.scss'],
   standalone: true,
-  imports: [CommonModule, LimitClicksDirective, NavigationComponent],
+  imports: [CommonModule, MButtonComponent, LimitClicksDirective, NavigationComponent],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DirectiveComponent {}
+export class DirectiveComponent {
+  readonly isButtonDisabled: WritableSignal<boolean> = signal(false);
+
+  onDisabledStateChanged(isDisabled: boolean): void {
+    this.isButtonDisabled.set(isDisabled);
+  }
+}
